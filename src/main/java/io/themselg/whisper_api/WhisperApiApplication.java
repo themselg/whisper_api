e7456637842a3b52711a5645bd2986a62cdcc9d0
Guillermo Themsel <themselg@icloud.com>
2025-11-08 16:03:26 -0600

    Initial commit

---
+++ src/main/java/io/themselg/whisper_api/WhisperApiApplication.java
@@ -0,0 +1,13 @@
+package io.themselg.whisper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhisperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/themselg/whisper_api/WhisperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.themselg.whisper_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
